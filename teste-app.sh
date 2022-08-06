2cb0f948818dd0c6d0a1f9e705e531c36f339750
fox-ban <108288391+fox-ban@users.noreply.github.com>
2022-08-06 14:02:30 -0300

    Alteração teste-app.sh

--- teste-app.sh
+++ teste-app.sh
@@ -1,10 +1,10 @@
 #/bin/bash
-RESULT=wget -qO- http://localhost:8090
+RESULT=$(wget -qO- http://localhost:8090)
 wget -q localhost:8090
 if [ $? -eq 0 ]
 then
     echo 'OK - Serviço no ar!'
-    echo 'OK - Number of visits:' $RESULT
+    echo 'OK - ' $RESULT
 else
     echo "Not OK - Number of visits!"
     exit 1
